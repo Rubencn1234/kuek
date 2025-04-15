af775b7b93264d7c021c252da67195035cf0ed4f
Rubencn1234 <rubencatril7@gmail.com>
2025-04-15 15:39:53 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,11 @@
+#print("Ingrese su nombre")
+#nombre=input()
+#print("Ingrese su edad")
+#edad=input()
+#print("Su nombre es:", nombre, "y su edad es", edad)
+
+
+n1=int(input("Ingrese un numero: "))
+n2=int(input("Ingrese otro numero: "))
+print(n1*n2)
+
---
+++ kuek.py
@@ -0,0 +1,3 @@
+n1=int(input("Ingrese un numero: "))
+n2=int(input("Ingrese otro numero: "))
+print("Resultado:", n1*n2)
